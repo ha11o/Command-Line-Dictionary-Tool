4e16d31b214da14e5188cb4b4ab406839c5086b4
Rajith <rajithjarang@gmail.com>
2019-12-27 21:59:11 +0530

    api for communicating with the api host

---
+++ api.js
@@ -0,0 +1,56 @@
+const request = require("request");
+const util    = require('util');
+const urljoin = require('url-join');
+var   config  = require('./config');
+
+
+// primisify the request
+const promiseRequest = util.promisify(request);
+
+
+//Get the api key and host form the config file
+const api_key  = config.api.key;
+const api_host = config.api.host;
+
+// {apihost}/words/randomWord?api_key={api_key}
+// {apihost}/word/{word}/definitions?api_key={api_key}
+// {apihost}/word/{word}/examples?api_key={api_key}
+// {apihost}/word/{word}/relatedWords?api_key={api_key}
+
+function UrlGenerator(dataTypeRequested,word=""){
+
+      if(dataTypeRequested === config.word.randomWord){
+
+          var url =  urljoin(api_host,"words",word,dataTypeRequested,"?api_key="+api_key);
+          return url;
+
+      }
+      else{
+
+          var url =  urljoin(api_host,"word",word,dataTypeRequested,"?api_key="+api_key);
+          return url;
+
+      }
+
+}
+
+
+//returns a promisifed request so handle it
+async function ApiRequest(url) {
+
+      return promiseRequest(url,{ json: true });
+}
+
+
+async function GetData(dataTypeRequested,word) {
+
+    var generatedUrl = UrlGenerator(dataTypeRequested,word);
+    return ApiRequest(generatedUrl);
+
+}
+
+module.exports = {
+
+    GetData:GetData
+
+};
