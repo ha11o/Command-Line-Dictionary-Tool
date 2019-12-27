f0b86ba69d6fc0e14c8fd429d2ddab78c5a4d7ba
Rajith <rajithjarang@gmail.com>
2019-12-27 20:12:30 +0530

    Added function place holders

---
+++ getdata.js
@@ -0,0 +1,46 @@
+const request = require("request");
+const util    = require('util');
+
+
+
+const promiseRequest = util.promisify(request);
+
+
+function GetDefination( word ) {
+
+
+
+}
+
+
+function GetSynonym(word) {
+
+
+
+}
+
+
+function GetAntonym(word) {
+
+
+
+}
+
+function GetExample(word){
+
+
+
+}
+
+
+
+function GetWordFullDict(word) {
+
+
+}
+
+
+function GetRandomWordFullDict(){
+
+
+}
